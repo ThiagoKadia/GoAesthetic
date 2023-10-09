4cad4b75108a84b4498fdefb2f294fa106bd81da
eduardols <edulssobrinho@hotmail.com>
2023-10-08 23:10:20 -0300

    Alterações na estrutura dos dados

--- Banco de Dados/DataBaseGoAesthetic/tabelas/tbAlimentos.sql
+++ Banco de Dados/DataBaseGoAesthetic/tabelas/tbAlimentos.sql
@@ -2,6 +2,6 @@
 (
 	[ALM_Id] INT NOT NULL PRIMARY KEY IDENTITY, 
     [IFA_Id] INT NOT NULL, 
-    [ALM_Quantidade] DECIMAL(5, 2) NULL,
+    [ALM_Quantidade] FLOAT NULL,
     CONSTRAINT [FK_tbAlimentos_tbInformacoesAlimentos] FOREIGN KEY ([IFA_Id]) REFERENCES [tbInformacoesAlimentos]([IFA_Id])
 )
--- Banco de Dados/DataBaseGoAesthetic/tabelas/tbMarcosEvolucao.sql
+++ Banco de Dados/DataBaseGoAesthetic/tabelas/tbMarcosEvolucao.sql
@@ -2,8 +2,8 @@
 (
 	[MEV_Id] INT NOT NULL PRIMARY KEY IDENTITY, 
     [USR_Id] INT NOT NULL, 
-    [MEV_Altura] DECIMAL(8, 3) NULL, 
-    [MEV_Peso] DECIMAL(8, 3) NULL, 
+    [MEV_Altura] FLOAT NULL, 
+    [MEV_Peso] FLOAT NULL, 
     [MEV_CaminhoFoto] VARCHAR(MAX) NULL, 
     [MEV_Data] DATETIME NULL, 
     CONSTRAINT [FK_tbMarcosEvolucao_tbUsuarios] FOREIGN KEY ([USR_Id]) REFERENCES [tbUsuarios]([USR_Id])
