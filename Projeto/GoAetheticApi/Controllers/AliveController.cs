b5ac76340ab5b6bfb24add9909f3a908c9170b77
Vittauumm <victoraugustoft00@outlook.com>
2023-10-30 21:28:41 -0300

    endpoint estou vivo

---
+++ Projeto/GoAetheticApi/Controllers/AliveController.cs
@@ -0,0 +1,21 @@
+﻿using GoAetheticApi.Models.Response;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GoAestheticApi.Controllers
+{
+    [Route("v1/api/is-alive")]
+    [ApiController]
+    public class AliveController : ControllerBase
+    {
+
+        [HttpGet]
+        public ActionResult EstouVivo()
+        {
+            return Ok(new BaseResponse()
+            {
+                Codigo = StatusCodes.Status200OK,
+                Mensagem = "Estou Vivo"
+            });
+        }
+    }
+}
