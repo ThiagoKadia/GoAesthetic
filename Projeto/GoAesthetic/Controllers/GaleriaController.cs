fe5573f7542e90fbe012313aa1912cc6258968cd
ADENILSON ALVES JUNIOR <081200026@faculdade.cefsa.edu.br>
2023-10-30 22:18:59 -0300

    Incluido tela de galeria

---
+++ Projeto/GoAesthetic/Controllers/GaleriaController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GoAesthetic.Controllers
+{
+    public class GaleriaController : Controller 
+    {
+        public IActionResult Index()
+        {
+            ViewBag.SideBar = true;
+            return View();
+        }
+    }
+}
