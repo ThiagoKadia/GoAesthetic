a25db3f2be3acd3519acdbed9347869c54109b22
ADENILSON ALVES JUNIOR <081200026@faculdade.cefsa.edu.br>
2023-10-30 21:54:11 -0300

    Tela de configurações inserida

---
+++ Projeto/GoAesthetic/Views/Configuracoes/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace GoAesthetic.Views.Configuracoes
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
