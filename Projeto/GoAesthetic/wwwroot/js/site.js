b3f05e1f38f5f4c2fbe408e716c4a212c509407d
eduardols <edulssobrinho@hotmail.com>
2023-11-04 22:10:08 -0300

    Logout

--- Projeto/GoAesthetic/wwwroot/js/site.js
+++ Projeto/GoAesthetic/wwwroot/js/site.js
@@ -28,7 +28,6 @@ document.addEventListener("DOMContentLoaded", function () {
                             window.location.href = "/Login";
                         }
                     });
-                    window.location.href = "/Login";
                 }
             });
         });
