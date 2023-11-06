0e74668a40135109e099d83733d31ee635baf1c5
ADENILSON ALVES JUNIOR <081200026@faculdade.cefsa.edu.br>
2023-11-06 09:34:08 -0300

    Correção de bugs: nosso contato

--- Projeto/GoAesthetic/wwwroot/js/nossoContato.js
+++ Projeto/GoAesthetic/wwwroot/js/nossoContato.js
@@ -1,18 +1,15 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const loginForm = document.getElementById('btn-secondary');
+    const btnFaleConosco = document.getElementById('btnFaleConosco');
 
-    loginForm.addEventListener('submit', function (e) {
-        e.preventDefault();
-
-            Swal.fire({
-                icon: 'info',
-                title: 'Nossos Contatos:',
-                text: 'E-mail: contato.goaesthetic@gmail.com Tel: (11) 94548-3720',
-                customClass: {
-                    popup: 'swal-custom-popup',
-                    content: 'swal-custom-text'
-                },
-            });
-
+    btnFaleConosco.addEventListener('click', function () {
+        Swal.fire({
+            icon: 'info',
+            title: 'Nossos Contatos:',
+            text: 'E-mail: contato.goaesthetic@gmail.com Tel: (11) 94548-3720',
+            customClass: {
+                popup: 'swal-custom-popup',
+                content: 'swal-custom-text'
+            },
+        });
     });
 });
